f883a434e38d9541bb30a832b71341c87624acf1
John Nkp <john.nkp1@gmail.com>
2025-08-23 16:01:28 +1000

    added production host url in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  # configure hosts in production
+  config.hosts << "quiz-lms-rails-production.up.railway.app"
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
