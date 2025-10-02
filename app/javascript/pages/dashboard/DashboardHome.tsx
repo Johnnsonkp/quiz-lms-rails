433d115abb3b69cf9f39ded4c7af546e5727d890
John Nkp <john.nkp1@gmail.com>
2025-10-03 00:38:06 +1000

    UI tweaks to landingpage and dashboard

--- app/javascript/pages/dashboard/DashboardHome.tsx
+++ app/javascript/pages/dashboard/DashboardHome.tsx
@@ -17,17 +17,13 @@ type DashboardHomeProps = {
 };
 
 export const DashboardHome = memo(({ user, dashboard_stats }: DashboardHomeProps) => {
-
+  console.log("user", user)
   return (
     <section className="space-y-6">
       <header className="flex justify-between items-center">
         <h1 className="text-left !text-xl font-bold text-gray-800">Welcome Back! {user?.name || user?.email}</h1>
       </header>
 
-      <DashboardHeatmap 
-        user={user}
-        activityType="quiz"
-      />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="bg-white p-4 rounded-lg shadow-md">
           <h2 className="text-lg font-semibold text-gray-700">Available Topics</h2>
@@ -45,7 +41,28 @@ export const DashboardHome = memo(({ user, dashboard_stats }: DashboardHomeProps
           <h2 className="text-lg font-semibold text-gray-700">Total Questions</h2>
           <p className="text-2xl font-bold text-blue-600">{dashboard_stats?.total_questions ?? 0}</p>
         </div>
+
+        <div className="flex justify-between bg-white p-4 rounded-lg shadow-md">
+          <div class="w-15 h-15 bg-indigo-600 rounded-full flex items-center justify-center">
+            <h3 class="text-white font-semibold text-2xl">
+              {user && user?.name[0]}
+            </h3>
+          </div>
+          <hr></hr>
+          <div class="flex-col">
+            <h2 className="text-sm font-semibold text-gray-700 pb-1">{user && user?.email}</h2>
+            <h2 className="text-sm font-normal text-gray-700">Active Streak:</h2>
+            <h2 className="text-sm font-normal text-gray-700">Level: 1</h2>
+          </div>
+
+        </div>
       </div>
+
+      <DashboardHeatmap 
+        user={user}
+        activityType="quiz"
+      />
+      
 
     </section>
   );
